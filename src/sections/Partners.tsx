90211ed1c7fda18a10b47e5f8ef1d3c3b9b1eed0
Mahmudul Hasan Shawon <mhshan177@gmail.com>
2026-09-04 16:52:30 +0600

    Narrow marquee edge fades on mobile

--- src/sections/Partners.tsx
+++ src/sections/Partners.tsx
@@ -11,8 +11,8 @@ export function Partners() {
         </Reveal>
       </div>
       <div className="relative overflow-hidden">
-        <div className="absolute left-0 top-0 bottom-0 w-[22rem] bg-gradient-to-r from-void to-transparent z-10" />
-        <div className="absolute right-0 top-0 bottom-0 w-[22rem] bg-gradient-to-l from-void to-transparent z-10" />
+        <div className="absolute left-0 top-0 bottom-0 w-24 md:w-[22rem] bg-gradient-to-r from-void to-transparent z-10" />
+        <div className="absolute right-0 top-0 bottom-0 w-24 md:w-[22rem] bg-gradient-to-l from-void to-transparent z-10" />
         <div className="marquee-track flex items-center w-max">
           {[...partners, ...partners].map((p, i) => (
             <div key={i} className="flex items-center pr-44">
